a08128b7fd66c946f2a68602d638562737fd5afc
Matt <matthew.finney@hotmail.com>
2026-02-12 21:09:54 -0500

    Practice #1 to correct syntax and mistakes

---
+++ src/CSC450Mod1/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,28 @@
+/* 
+   Matthew Finney
+   CSC450 - Module 1 program corrections #1
+
+   Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.
+*/
+
+#include <iostream>
+#include <conio.h>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+   // Standard Output Statement
+   cout << "Welcome to this C++ Program." << endl;
+   cout << "I have corrected all errors for this program." << endl;
+
+   // Wait For Output Screen
+   cout << "Press any key to continue...";
+   _getch();
+
+   //Main Function return Statement
+   return 0;
+}
